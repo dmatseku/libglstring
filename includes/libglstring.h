adbf067393f69b37fe5216bfaf4309eb013a25d4
dmatseku <kamikoto.sudzuki@gmail.com>
2019-08-08 16:56:09 +0300

    header correcting

--- includes/libglstring.h
+++ includes/libglstring.h
@@ -3,7 +3,7 @@
 
 char	string_create(char* str, float x, float y, int pix_size);
 
-char	string_init(char* const dir, GLFWwindow* const window);
+char	string_init(char* const shader_dir, char* font, GLFWwindow* const window);
 
 void	string_delete_one(size_t index);
 
